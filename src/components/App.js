412d0cec0ac4d193b223e9eb0a1d21bfdccc22da
Allu Hanuma Reddy <82640761+HanumaAllu@users.noreply.github.com>
2024-11-25 11:11:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  
+  const handleInputChange = (event) => {
+    setInput(event.target.value);  
+  }
+
+ 
+  const handleAddTask = () => {
+    if (input.trim() !== '') {
+      setTasks([...tasks, input]);  
+      setInput(''); 
+    }
+  }
+
+  const handleDeleteTask = (index) => {
+    const newTasks = tasks.filter((task, taskIndex) => taskIndex !== index); 
+    setTasks(newTasks);  
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div className="todo-container">
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Enter a task"
+        />
+        <button onClick={handleAddTask}>Add Todo</button>
+      </div>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
